4fdc51fd4c8b7987e587d54e4290785b42054286
Megan <esler6724@gmail.com>
2022-11-28 13:22:06 -0800

    commit tree cd55ef344b1e10f33fe02853992e16e891b56b92

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+addingEventListener()
+
+// const input = document.getElementById(`button`);
+// input.addEventListener(`click`, function() {
+//   alert(`I was clicked!`);
+// })
